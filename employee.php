be24988c79749f9af70b4e5b0acdc82ccd9c6a5d
ntaweera <taweerat.nu@skru.ac.th>
2020-10-05 12:42:06 +0700

    Add files via upload
    add update action

--- employee.php
+++ employee.php
@@ -1,6 +1,10 @@
 <?php include("connect.php"); 
-$emp_id = $_POST["emp_id"];
-if(isset($emp_id)){
+
+// insert data
+
+$action = $_REQUEST['action']?'updateform':'initform';
+if($action=="insert"){
+    $emp_id = $_POST["emp_id"];
     $emp_name = $_POST["emp_name"];
     $gender = $_POST["gender"];
     $dept_id = $_POST["dept_id"];
@@ -16,8 +20,104 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
 }
-
 ?>
+
+<?php
+// update data
+if($action=="updateform"){  
+    $emp_id = $_GET["emp_id"];
+    $sql = "select * from employee where emp_id = '$emp_id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $emp_name = $row["emp_name"];
+    $gender = $row["gender"];
+    $dept_id = $row["dept_id"];
+    $work_type_id = $row["work_type_id"];
+    $emp_type_id = $row["emp_type_id"];
+    
+?>
+    <body>
+<h1>แก้ไขข้อมูลพนักงาน</h1>
+<p>เมนูข้อมูลพนักงาน</p>
+<form method="post">
+  <input type="hidden"  name="action" value="updatedata">
+  <label for="emp_id">รหัสพนักงาน:</label>
+  <input type="text" id="emp_id" name="emp_id" value="<?=$emp_id?>"><br>
+  <label for="emp_name">ชื่อ-นามสกุล:</label>
+  <input type="text" id="emp_name" name="emp_name" value="<?=$emp_name ?>"><br>
+  <label for="gender">เพศ:</label>
+  <select name="gender" id="gender">
+  <?php
+    if($gender=="M"){  
+    ?>
+    <option value="M" selected>ชาย</option>
+    <option value="F">หญิง</option>
+
+    <?php }else{ ?>
+        <option value="M" >ชาย</option>
+        <option value="F" selected>หญิง</option>
+
+     <?php }  ?> 
+
+   </select><br>
+    <label for="dept_id">แผนก/ฝ่าย:</label>
+  <select name="dept_id" id="dept_id">
+    <?php       
+        $sql = "select * from department";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <option value="<?=$row["dept_id"]?>" <?php if($dept_id==$row["dept_id"]) echo "selected" ?> ><?=$row["dept_name"]?></option>
+            <?php
+            }  //end while loop
+        }  //end if
+    ?> 
+   </select><br>
+
+   <label for="work_type_id">กะ:</label>
+  <select name="work_type_id" id="work_type_id">
+    <?php
+        
+        $sql = "select * from work_type";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <option value="<?=$row["work_type_id"]?>" <?php if($row["work_type_id"]==$work_type_id) echo "selected"  ?> ><?=$row["work_type_name"]?></option>
+
+            <?php
+            }  //end while loop
+        }  //end if
+    ?> 
+   </select><br>
+   <label for="emp_type_id">รายวัน/รายเดือน:</label>
+  <select name="emp_type_id" id="emp_type_id">
+    <?php
+        
+        $sql = "select * from emp_type";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <option value="<?=$row["emp_type_id"]?>" <?php if($row["emp_type_id"]==$emp_type_id) echo "selected" ?> ><?=$row["emp_type"]?></option>
+
+            <?php
+            }  //end while loop
+        }  //end if
+        //mysqli_close($conn);
+    ?> 
+   </select><br>
+   <input type="submit" value="แก้ไข">
+</form>
+
+<?php 
+} 
+?>
+
 <!DOCTYPE HTML>
 <html>
 <meta charset="utf-8">
@@ -30,7 +130,10 @@ table, th, td {
 </style>
     <title>TUS</title>
 <body>
+
+
 <h1>สร้างข้อมูลพนักงาน</h1>
+<?php if($action=='initform'){ ?>
 <p>เมนูข้อมูลพนักงาน</p>
 <form method="post">
   <input type="hidden"  name="action" value="insert">
@@ -97,6 +200,7 @@ table, th, td {
    </select><br>
    <input type="submit" value="เพิ่ม">
 </form>
+    <?php } ?>
 <?php
     $sql = "SELECT e.emp_id, e.emp_name, e.gender, d.dept_name,
     w.work_type_name, et.emp_type 
@@ -116,6 +220,8 @@ table, th, td {
     <th>แผน/ฝ่าย</th>
     <th>กะ</th>
     <th>รายวัน/รายเดือน</th>
+    <th>แก้ไข</th>
+    <th>ลบ</th>
   </tr>
   <?php  
         if (mysqli_num_rows($result) > 0) {
@@ -134,6 +240,8 @@ table, th, td {
     <td><?=$row["dept_name"]?></td>
     <td><?=$row["work_type_name"]?></td>
     <td><?=$row["emp_type"]?></td>
+    <td><a href="employee.php?emp_id=<?=$row["emp_id"]?>&action=updateform">แก้ไข</a></td>
+    <td>ลบ</td>
   </tr>
   <?php 
             } // end while
